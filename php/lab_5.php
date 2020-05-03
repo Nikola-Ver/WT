8aabad198a8a4ad4cf6c93be47a3ccefe21b972f
Nikola-Ver <46997500+Nikola-Ver@users.noreply.github.com>
2020-05-03 15:44:33 +0300

    Lab 5

---
+++ php/lab_5.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MySQL & PHP</title>
+    <link rel="shortcut icon" href="../picture/MainIcon.png" type="image/x-icon" />
+    <link rel="stylesheet" href="../css/main.css" />
+    <script src="../js/hat.js"></script>
+    <script src="../js/registration.js"></script>
+</head>
+
+<body>
+    <?php
+    include 'main_elements.php';
+    echo $main_elements;
+    ?>
+</body>
+
+</html>
