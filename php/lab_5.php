cd0102b758348e0d1138f6e16017eecf2955453c
Nikola-Ver <46997500+Nikola-Ver@users.noreply.github.com>
2020-05-03 16:19:24 +0300

    Add divs

--- php/lab_5.php
+++ php/lab_5.php
@@ -7,6 +7,7 @@
     <title>MySQL & PHP</title>
     <link rel="shortcut icon" href="../picture/MainIcon.png" type="image/x-icon" />
     <link rel="stylesheet" href="../css/main.css" />
+    <link rel="stylesheet" href="../css/lab_5.css" />
     <script src="../js/hat.js"></script>
     <script src="../js/registration.js"></script>
 </head>
@@ -16,6 +17,16 @@
     include 'main_elements.php';
     echo $main_elements;
     ?>
+
+    <div class="main_div">
+        <div class="input_sql">
+
+        </div>
+
+        <div class="output_sql">
+
+        </div>
+    </div>
 </body>
 
 </html>
