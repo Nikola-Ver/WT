93ec1f88ef00502c67403ca5526caa73cd753710
Nikola-Ver <46997500+Nikola-Ver@users.noreply.github.com>
2020-05-04 22:42:17 +0300

    Changed style and added new cells

--- php/lab_7.php
+++ php/lab_7.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="../css/lab_7.css" />
     <script src="../js/hat.js"></script>
     <script src="../js/registration.js"></script>
+    <?php include 'lab_7_logic.php'; ?>
 </head>
 
 <body>
@@ -18,7 +19,9 @@
     echo $main_elements;
     ?>
 
-    <form action="lab_7_logic.php" class="input_div" method="post">
+    <form action="lab_7.php" class="input_div" method="post">
+        <input name="email_sender" type="email" class="input_text" placeholder="Email отправителя">
+        <input name="name" type="text" class="input_text" placeholder="Имя отправителя">
         <input name="email" type="email" class="input_text" placeholder="Email получателя">
         <input name="subject" type="text" class="input_text" placeholder="Тема письма">
         <textarea name="text" class="input_text" placeholder="Введите письмо..."></textarea>
--- php/lab_7_logic.php
+++ php/lab_7_logic.php
@@ -1,12 +1,7 @@
 <?php
-$back_url = $_SERVER['HTTP_REFERER'];
-
+error_reporting(0);
 $msg = $_POST['text'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
 $emailSender = "";
 $name = "";
-
-
-header("Refresh: 0; '$back_url'");
-exit();
